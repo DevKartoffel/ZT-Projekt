554df3bab5901ded2e1183f5942d7c18d459e9df
Steven <steven.gallas@haw-hamburg.de>
2021-04-07 19:12:11 +0200

    $ mkdir repo $ cd repo $ git init Initialized empty Git repository in /home/phihag/repo/.git/ $ git config --global user.email "me@example.com" $ git config --global user.name "My Name" $ sudo git commit -m first
    *** Please tell me who you are.
    
    Run
    
      git config --global user.email "Steven.gallas@haw-hamburg.de"
      git config --global user.name "Steven"
    
    to set your account's default identity.
    Omit --global to set the identity only in this repository.
    
    fatal: unable to auto-detect email address (got 'root@t4.(none)')

---
+++ Matlab/Praktikum_1.m
@@ -0,0 +1,37 @@
+
+
+clear all;
+clc;
+
+D=0.017;
+R=0.02;
+m=0.27;
+r=sqrt(R^2-(D/2)^2);
+Ib=4.32*10^-5;
+Iw=0.14025;
+l=49;
+b=1;
+K=0.001;
+
+ut=0;
+xt=0;
+
+X0t=0;
+X1t=0;
+
+A0t=0;
+A1t=0;
+
+
+% Zustandsraum Elemente A,b,c,d
+
+A=
+
+b=
+
+c=
+
+d=
+
+
+
